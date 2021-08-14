4294ab275607154c937e98f598987a03d1d56b46
derselbst <tom.mbrt@googlemail.com>
2021-08-14 21:55:33 +0200

    Change opacity of ExifOverlay on mouseOver

--- ExifOverlay.hpp
+++ ExifOverlay.hpp
@@ -5,12 +5,17 @@
 #include "MessageWidget.hpp"
 
 class ExifWrapper;
+class QEnterEvent;
+class QEvent;
 
 class ExifOverlay : public MessageWidget
 {
 public:
     ExifOverlay(QWidget* parent = nullptr);
     ~ExifOverlay() override;
+    
+    void enterEvent(QEnterEvent* event) override;
+    void leaveEvent(QEvent* event) override;
 
     void setMetadata(ExifWrapper*);
 
