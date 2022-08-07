9ad1e905c014e36b0443b39a63e7b6290ba8f601
derselbst <tom.mbrt@googlemail.com>
2022-08-07 22:31:45 +0200

    fix build

--- src/widgets/ThumbnailListView.hpp
+++ src/widgets/ThumbnailListView.hpp
@@ -36,7 +36,6 @@ public:
 protected:
     void wheelEvent(QWheelEvent *event) override;
     void setSelection(const QRect &rect, QItemSelectionModel::SelectionFlags flags) override;
-    void mousePressEvent(QMouseEvent *event) override;
     
 private:
     struct Impl;
