d7487cb9fc0e416c065571113813fb7ddf40dfa3
derselbst <tom.mbrt@googlemail.com>
2023-03-12 17:39:41 +0100

    continue

--- src/decoders/CziDecoder.cpp
+++ src/decoders/CziDecoder.cpp
@@ -29,199 +29,22 @@ struct PageInfo
     }
 };
 
-constexpr const char TiffModule[] = "CziDecoder";
-
-// Note: a lot of the code has been taken from: 
-// https://github.com/qt/qtimageformats/blob/c64f19516dd2467bf5746eb24afe883bdbc15b25/src/plugins/imageformats/tiff/qtiffhandler.cpp
-
 struct CziDecoder::Impl
 {
     CziDecoder* q;
-    
-    TIFF* tiff = nullptr;
-    const unsigned char* buffer = nullptr;
-    qint64 offset = 0;
-    qint64 nbytes = 0;
-    
+
+    std::shared_ptr<libCZI::ICZIReader> cziReader;
+    std::shared_ptr<const void> pseudoSharedDataPtr;
+        
     std::vector<PageInfo> pageInfos;
 
     Impl(CziDecoder* q) : q(q)
     {
-        // those are nasty global, non-thread-safe functions. setting custom handlers will also affect QT's internal QImage decoding.
-        TIFFSetErrorHandler(nullptr);
-        TIFFSetWarningHandler(nullptr);
-        TIFFSetErrorHandlerExt(myErrorHandler);
-        TIFFSetWarningHandlerExt(myWarningHandler);
     }
     
-    static void myErrorHandler(thandle_t self, const char* module, const char* fmt, va_list ap)
-    {
-        if(::strcmp(module, TiffModule) != 0)
-        {
-            // Seems like this call was made from QT's internal decoding. self points to something else. Return.
-            return;
-        }
-        
-        auto impl = static_cast<Impl*>(self);
-        char buf[4096];
-        std::vsnprintf(buf, sizeof(buf)/sizeof(*buf), fmt, ap);
-
-        Formatter f;
-        if(module)
-        {
-            f << "Error in module '" << module << "': ";
-        }
-        f << buf;
-        
-        impl->q->setDecodingMessage(QString(f.str().c_str()));
-    }
-    
-    static void myWarningHandler(thandle_t self, const char* module, const char* fmt, va_list ap)
-    {
-        if(::strcmp(module, TiffModule) != 0)
-        {
-            // Seems like this call was made from QT's internal decoding. self points to something else. Return.
-            return;
-        }
-        
-        auto impl = static_cast<Impl*>(self);
-        char buf[4096];
-        std::vsnprintf(buf, sizeof(buf)/sizeof(*buf), fmt, ap);
-
-        Formatter f;
-        if(module)
-        {
-            f << "Warning in module '" << module << "': ";
-        }
-        f << buf;
-        
-        impl->q->setDecodingMessage(QString(f.str().c_str()));
-    }
-    
-    size_t convert32BitOrder(uint32_t *__restrict dst, const uint32_t *__restrict src, quint32 rows, quint32 width)
-    {
-        // swap rows from bottom to top
-        size_t to = 0;
-        for (qint64 r = rows; r>0; r--)
-        {
-            for (quint32 c=0; c<width; c++)
-            {
-                uint32_t p = src[(r-1)*width + c];
-                // convert between ARGB and ABGR
-                dst[to++] = TIFFGetA(p) << 24 |
-                            TIFFGetR(p) << 16 |
-                            TIFFGetG(p) <<  8 |
-                            TIFFGetB(p);
-            }
-            q->cancelCallback();
-        }
-        
-        return to;
-    }
-    
-    static tsize_t qtiffReadProc(thandle_t fd, tdata_t buf, tsize_t size)
-    {
-        auto impl = static_cast<Impl*>(fd);
-        
-        if(impl->offset >= impl->nbytes)
-        {
-            return 0;
-        }
-        else if(impl->offset + size < impl->nbytes)
-        {
-            
-        }
-        else
-        {
-            size = impl->nbytes - impl->offset;
-        }
-        
-        memcpy(buf, &impl->buffer[impl->offset], size);
-        
-        impl->offset += size;
-        
-        return size;
-    }
-
-    static tsize_t qtiffWriteProc(thandle_t, tdata_t, tsize_t)
-    {
-        return 0;
-    }
-
-    static toff_t qtiffSeekProc(thandle_t fd, toff_t off, int whence)
-    {
-        auto impl = static_cast<Impl*>(fd);
-        
-        switch (whence) {
-        case SEEK_SET:
-            impl->offset = off;
-            break;
-        case SEEK_CUR:
-            impl->offset += off;
-            break;
-        case SEEK_END:
-            impl->offset = impl->nbytes + off;
-            break;
-        }
-
-        if(impl->offset >= impl->nbytes)
-        {
-            return -1;
-        }
-        
-        return impl->offset;
-    }
-
-    static int qtiffCloseProc(thandle_t /*fd*/)
-    {
-        return 0;
-    }
-
-    static toff_t qtiffSizeProc(thandle_t fd)
-    {
-        return static_cast<Impl*>(fd)->nbytes;
-    }
-
-    static int qtiffMapProc(thandle_t /*fd*/, tdata_t* /*pbase*/, toff_t* /*psize*/)
-    {
-        return 0;
-    }
-
-    static void qtiffUnmapProc(thandle_t /*fd*/, tdata_t /*base*/, toff_t /*size*/)
-    {
-    }
-
     std::vector<PageInfo> readPageInfos()
     {
-        auto currentDirectory = TIFFCurrentDirectory(this->tiff);
-        
         std::vector<PageInfo> pageInfos;
-        do
-        {
-            pageInfos.emplace_back(PageInfo{});
-            
-            auto& info = pageInfos.back();
-            
-            if (!TIFFGetField(tiff, TIFFTAG_IMAGEWIDTH, &info.width) ||
-                !TIFFGetField(tiff, TIFFTAG_IMAGELENGTH, &info.height))
-            {
-                throw std::runtime_error("Error while reading TIFF dimensions");
-            }
-            
-            if(!TIFFGetField(tiff, TIFFTAG_PLANARCONFIG, &info.config) ||
-               !TIFFGetField(tiff, TIFFTAG_BITSPERSAMPLE, &info.bps) ||
-               !TIFFGetField(tiff, TIFFTAG_SAMPLESPERPIXEL, &info.spp))
-            {
-               throw std::runtime_error("Error while reading TIFF tags");
-            }
-        } while(TIFFReadDirectory(tiff));
-        
-        
-        if(!TIFFSetDirectory(this->tiff, currentDirectory))
-        {
-            throw std::runtime_error("This should never happen: TIFFSetDirectory failed to restore previous directory");
-        }
-        
         return pageInfos;
     }
 
@@ -291,7 +114,10 @@ struct CziDecoder::Impl
 };
 
 CziDecoder::CziDecoder(QSharedPointer<Image> image) : SmartImageDecoder(image), d(std::make_unique<Impl>(this))
-{}
+{
+    d->cziReader = libCZI::CreateCZIReader();
+    
+}
 
 CziDecoder::~CziDecoder()
 {
@@ -300,18 +126,29 @@ CziDecoder::~CziDecoder()
 
 void CziDecoder::close()
 {
-    if (d->tiff)
-    {
-        TIFFClose(d->tiff);
-    }
-    d->tiff = nullptr;
-    d->buffer = nullptr;
-    
+    d->pseudoSharedDataPtr = nullptr;
+    d->cziReader->Close();
     SmartImageDecoder::close();
 }
 
 void CziDecoder::decodeHeader(const unsigned char* buffer, qint64 nbytes)
 {
+    // construct a shared_ptr without deleter
+    d->pseudoSharedDataPtr.reset(buffer, [](void*) {});
+
+    auto inputStream = libCZI::CreateStreamFromMemory(d->pseudoSharedDataPtr, nbytes);
+
+    d->cziReader->Open(inputStream);
+    auto metadataSegment = d->cziReader->ReadMetadataSegment();
+
+    const auto metadata = metadataSegment->CreateMetaFromMetadataSegment();
+    const auto docInfo = metadata->GetDocumentInfo();
+    auto displaySettings = docInfo->GetDisplaySettings();
+
+    auto stat = d->cziReader->GetStatistics();
+    auto pyramidStat= d->cziReader->GetPyramidStatistics();
+
+
     d->buffer = buffer;
     d->nbytes = nbytes;
     d->offset = 0;
