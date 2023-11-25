640cf9e3b5b31f849893fa66c7c395f494af3058
derselbst <tom.mbrt@googlemail.com>
2023-11-25 22:36:37 +0100

    Fix kdcraw being unable to open some RAW files
    kdcraw might be unable to open a RAW file if it is located in a folder which has special characters. Might be an encoding issue somewhere. Only Release builds on Windows were affected. Get rid of all that filepath conversion stuff and use LibRaw directly to open the existing file buffer.

---
+++ src/decoders/LibRawHelper.cpp
@@ -0,0 +1,60 @@
+
+#include "LibRawHelper.hpp"
+
+#include "Formatter.hpp"
+
+// libraw basically include the entire Windoof API, messing up any C++ source code that uses std::min
+#include <libraw.h>
+#include <libraw_version.h>
+
+#ifdef LIBRAW_HAS_CONFIG
+#include <libraw_config.h>
+#endif
+
+
+void LibRawHelper::extractThumbnail(QByteArray& encodedThumbnailOut, const void* fileBuf, qint64 buflen)
+{
+	LibRaw raw;
+            
+	int ret = raw.open_buffer(const_cast<void*>(fileBuf), buflen);
+	if (ret != LIBRAW_SUCCESS)
+	{
+		throw std::runtime_error(Formatter () << "LibRaw: failed to run open_buffer: " << libraw_strerror(ret));
+	}
+
+	auto loadEmbeddedPreview = [](QByteArray& imgData, LibRaw& raw)
+	{
+		int ret = raw.unpack_thumb();
+
+		if (ret != LIBRAW_SUCCESS)
+		{
+			throw std::runtime_error(Formatter() << "LibRaw: failed to run unpack_thumb: " << libraw_strerror(ret));
+		}
+
+		libraw_processed_image_t* const thumb = raw.dcraw_make_mem_thumb(&ret);
+		if (!thumb)
+		{
+			throw std::runtime_error(Formatter() << "LibRaw: failed to run dcraw_make_mem_thumb: " << libraw_strerror(ret));
+		}
+
+		if (thumb->type == LIBRAW_IMAGE_JPEG)
+		{
+			imgData = QByteArray((const char*)thumb->data, (int)thumb->data_size);
+		}
+		else
+		{
+			raw.dcraw_clear_mem(thumb);
+			throw std::runtime_error("LibRaw returned a non-JPEG thumbnail, which is currently not supported, sry.");
+		}
+
+		raw.dcraw_clear_mem(thumb);
+		raw.recycle();
+
+		if (imgData.isEmpty())
+		{
+			throw std::runtime_error(Formatter() << "JPEG thumb from LibRaw is empty!");
+		}
+	};
+
+	loadEmbeddedPreview(encodedThumbnailOut, raw);
+}
