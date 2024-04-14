6dc070b1814990c117c2e17c2254d951d722916e
derselbst <tom.mbrt@googlemail.com>
2024-04-14 19:14:19 +0200

    uncomment

--- src/decoders/DecoderFactory.cpp
+++ src/decoders/DecoderFactory.cpp
@@ -125,7 +125,7 @@ std::unique_ptr<SmartImageDecoder> DecoderFactory::getDecoder(const QSharedPoint
         }
         else if(format == "jxl")
         {
-        //     return std::make_unique<SmartJxlDecoder>(image);
+            return std::make_unique<SmartJxlDecoder>(image);
         }
         
         return std::make_unique<MangoDecoder>(image);
